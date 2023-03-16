40fe18530f2248a1cc1b580788fce9b3d663fafa
cristopher ontiveros <christoph.alano@gmail.com>
2023-03-15 23:29:11 -0500

    added imports

--- application/app/__init__.py
+++ application/app/__init__.py
@@ -1,9 +1,12 @@
-
 import os
 import logging
 from logging.handlers import RotatingFileHandler
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 from config import Config
+from flask_wtf.csrf import CSRFProtect
 
 db = SQLAlchemy()
 migrate = Migrate()
