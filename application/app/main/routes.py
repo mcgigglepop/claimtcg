ecf17db926237c4f084a700950ad31331191ef2b
cristopher ontiveros <christoph.alano@gmail.com>
2023-03-16 00:57:05 -0500

    Route and method for rendering the about page.

--- application/app/main/routes.py
+++ application/app/main/routes.py
@@ -15,3 +15,10 @@ def dashboard():
     Route and method for rendering the dashboard page.
     """
     return render_template('internal/dashboard.html', title='Dashboard')
+
+@bp.route('/about', methods=['GET', 'POST'])
+def about():
+    """
+    Route and method for rendering the about page.
+    """
+    return render_template('customer_facing/about.html', title='About Us')
