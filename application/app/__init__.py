499676889e6587753dc62775d8105a5f84e6150a
cristopher ontiveros <christoph.alano@gmail.com>
2023-03-15 23:32:57 -0500

    added blueprint import in the application init

--- application/app/__init__.py
+++ application/app/__init__.py
@@ -25,6 +25,15 @@ def create_app(config_class=Config):
     csrf = CSRFProtect()
     csrf.init_app(app)
 
+    from app.errors import bp as errors_bp
+    app.register_blueprint(errors_bp)
+    
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
     if not app.debug and not app.testing:
         if not os.path.exists('logs'):
             os.mkdir('logs')
