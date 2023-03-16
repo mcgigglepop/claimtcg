be000613d29ec926907830e884d40076331fb700
cristopher ontiveros <christoph.alano@gmail.com>
2023-03-16 00:11:04 -0500

    generate the userid hash

--- application/app/auth/routes.py
+++ application/app/auth/routes.py
@@ -4,6 +4,7 @@ from flask import request, redirect, render_template, url_for, flash
 from flask_login import current_user
 from app import db
 from app.models import User
+import uuid
 
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
@@ -36,13 +37,16 @@ def register():
         password = request.form.get('password')
         confirmPassword = request.form.get('confirmPassword')
 
-    # check if passwords match
+        # check if passwords match
         if password != confirmPassword: 
             flash('Passwords do not match')
             return redirect(url_for('auth.register'))
         
-    # check if the email address exists already
+        # check if the email address exists already
         user = User.query.filter_by(email=email).first()
         if user is not None:
             flash('This email address already exists. Please use a different email address.')
             return redirect(url_for('auth.register'))
+        
+        # generate the userid hash
+        userIdHashash = uuid.uuid4().hex
