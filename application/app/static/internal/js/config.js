d2f8c6a021fa84fb4acee595a690bf034c5e2118
cristopher ontiveros <christoph.alano@gmail.com>
2023-03-24 23:32:28 -0500

    added config for poller and websocket

---
+++ application/app/static/internal/js/config.js
@@ -0,0 +1,11 @@
+window._config = {
+    cognito: {
+        userPoolId: 'NAN',
+        userPoolClientId: 'NAN',
+        region: 'NAN' 
+    },
+    api: {
+        pollerInvokeUrl: '',
+        wsInvokeUrl: '' 
+    }
+};
