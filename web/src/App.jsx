b2f513c78a60fa11a20ca22cde64a9d73a1b012e
cristopher ontiveros <christoph.alano@gmail.com>
2024-01-19 00:13:37 -0600

    added trending products

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,5 +1,6 @@
 import Header from './components/Header/Header.jsx';
 import Hero from './components/Hero/Hero.jsx';
+import TrendingProducts from './components/TrendingProducts/TrendingProducts.jsx';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div class="no-bottom no-top" id="content">
         <div id="top"></div>
           <Hero />
+          <TrendingProducts />
       </div>
     </div>
   );
