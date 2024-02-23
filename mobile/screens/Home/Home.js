7cfd1d44c767c22fc1cffcc6c1b69748ffbf25ab
cristopher <christoph.alano@gmail.com>
2024-02-23 17:59:36 -0600

    added search componenet

--- mobile/screens/Home/Home.js
+++ mobile/screens/Home/Home.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native';
-import Header from '../../components/Header/Header';
-import Tab from '../../components/Tab/Tab';
-
+import Search from '../../components/Search/Search';
 import style from './style';
 import globalStyle from '../../assets/styles/globalStyle';
-import Badge from '../../components/Badge/Badge';
 
 const Home = () => {
   return (
     <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
-      <Header title={'Azzahri A.'} type={1} />
-      <Tab title={'Highlight'} />
-      <Tab title={'Highlight'} isInactive={true} />
-      <Badge title={'Environment'} />
+      <Search
+        onSearch={value => {
+          console.log(value);
+        }}
+      />
     </SafeAreaView>
   );
 };
