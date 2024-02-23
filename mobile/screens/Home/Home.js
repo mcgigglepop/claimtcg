7aea2c5b8529aa45692ab09e438c7d07725f132f
cristopher <christoph.alano@gmail.com>
2024-02-23 15:40:33 -0600

    added tab

--- mobile/screens/Home/Home.js
+++ mobile/screens/Home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native';
 import Header from '../../components/Header/Header';
-import Button from '../../components/Button/Button';
+import Tab from '../../components/Tab/Tab';
 
 import style from './style';
 import globalStyle from '../../assets/styles/globalStyle';
@@ -10,13 +10,8 @@ const Home = () => {
   return (
     <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
       <Header title={'Azzahri A.'} type={1} />
-      <Button
-        title={'Donate'}
-        onPress={() => {
-          console.log('pressed');
-        }}
-      />
-      <Button title={'donate'} isDisabled={true} />
+      <Tab title={'Highlight'} />
+      <Tab title={'Highlight'} isInactive={true} />
     </SafeAreaView>
   );
 };
