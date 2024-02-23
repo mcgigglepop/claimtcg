0350dcfd8e55467838881efa2bc8a1f116f8d4f8
cristopher <christoph.alano@gmail.com>
2024-02-23 15:46:15 -0600

    added badges

--- mobile/screens/Home/Home.js
+++ mobile/screens/Home/Home.js
@@ -5,6 +5,7 @@ import Tab from '../../components/Tab/Tab';
 
 import style from './style';
 import globalStyle from '../../assets/styles/globalStyle';
+import Badge from '../../components/Badge/Badge';
 
 const Home = () => {
   return (
@@ -12,6 +13,7 @@ const Home = () => {
       <Header title={'Azzahri A.'} type={1} />
       <Tab title={'Highlight'} />
       <Tab title={'Highlight'} isInactive={true} />
+      <Badge title={'Environment'} />
     </SafeAreaView>
   );
 };
